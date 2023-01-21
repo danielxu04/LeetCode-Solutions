b6202694f80f982f29855a35a4d4dfe1c3d8c89d
Daniel Xu <danielxu004@gmail.com>
2023-01-21 03:36:21 -0500

    Hard - Substring with Concatenation of All Words

---
+++ Solutions/substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+       vector<int> ans;
+        int n = s.size(), cnt = words.size();
+        if (n <= 0 || cnt <= 0) return ans;
+        
+        // init word occurence
+        unordered_map<string, int> dict;
+        for (int i = 0; i < cnt; ++i) dict[words[i]]++;
+        
+        // travel all sub string combinations
+        int wl = words[0].size();
+        for (int i = 0; i < wl; ++i) {
+            int left = i, count = 0;
+            unordered_map<string, int> tdict;
+            for (int j = i; j <= n - wl; j += wl) {
+                string str = s.substr(j, wl);
+                // a valid word, accumulate results
+                if (dict.count(str)) {
+                    tdict[str]++;
+                    if (tdict[str] <= dict[str]) 
+                        count++;
+                    else {
+                        // a more word, advance the window left side possiablly
+                        while (tdict[str] > dict[str]) {
+                            string str1 = s.substr(left, wl);
+                            tdict[str1]--;
+                            if (tdict[str1] < dict[str1]) count--;
+                            left += wl;
+                        }
+                    }
+                    // come to a result
+                    if (count == cnt) {
+                        ans.push_back(left);
+                        // advance one word
+                        tdict[s.substr(left, wl)]--;
+                        count--;
+                        left += wl;
+                    }
+                }
+                // not a valid word, reset all vars
+                else {
+                    tdict.clear();
+                    count = 0;
+                    left = j + wl;
+                }
+            }
+        }
+        return ans;
+    }
+};
