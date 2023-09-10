8997c6d1790b3f04b34f4bcedec4a0e981aa7ea5
Daniel Xu <danielxu004@gmail.com>
2023-09-10 10:58:28 -0400

    Added Longest Consecutive Sequence solution

--- Solutions/longest-consecutive-sequence.cpp
+++ Solutions/longest-consecutive-sequence.cpp
@@ -64,4 +64,20 @@ public:
 
         return maxLength;
     }
+
+    // Set solution
+    int longestConsecuiveSet(vector<int> &nums) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        int longest = 0;
+        for (auto &n: s) {
+            if (!s.count(n - 1)) {
+                int length = 1;
+                while (s.count(n + length)) {
+                    length++;
+                }
+                longest = max(longest, length);
+            }
+        }
+        return longest;
+    }
 };
